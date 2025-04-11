5f5ad0a6a8d2bfabc67b2d77db9ac97fb38155e5
siyao.chen92 <siyao.chen92@gmail.com>
2025-04-11 03:03:59 +0000

    Set up the project

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyA-qhD7DIg_1ZFzIQuvojymC4eu9grttqM',
+      authDomain: 'sample-firebase-ai-app-23983.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'sample-firebase-ai-app-23983',
+      storageBucket: 'sample-firebase-ai-app-23983.firebasestorage.app',
+      messagingSenderId: '434449491430',
+      appId: '1:434449491430:web:522062d5882128869f6227',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyBZVfGUTJaiXppK2Jy-EnOPWQm05n97eUg',
+  debug_token: ''
+};
